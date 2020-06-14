4adf0a708fd1f0a3ead8c5d90c5b549fa1ac4a4d
Justtuchthat <rutgerwuijster@hotmail.com>
2020-06-14 23:40:23 +0200

    inputHelper update
    updated the key input file and main.lua for testing

--- gameFiles/PoolBuilderSim/inputHelper.lua
+++ gameFiles/PoolBuilderSim/inputHelper.lua
@@ -1,12 +1,23 @@
-local keysPressed = {{}, {}, {}}
-local mouse = {}
+-- inputHelper.lua
+-- created by Justtuchthat
+-- last updated on 14th of june 2020 with love2d version 11.3
+-- this program should help with getting the input of keys on the keyboard
+
+-- this list is the list that stated what keys to keep track of
+-- I recommend to delete the keys you don't need
+-- if you require aditional keys visit this site to see which keys are supported
+-- https://love2d.org/wiki/Scancode
+
+-- the keycode for us layout backslashes was being read correctly
+-- however this could not be used since lua sees \ as the start of special chars
 
 local keys = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3',
 '4', '5', '6', '7', '8', '9', '0', "return", "escape", "backspace", "tab",
-"space", '-', '=', '[', ']', '\\', "nonus#", ';', "'", '`', ',', '.', '/',
+"space", '-', '=', '[', ']', "nonus#", ';', "'", '`', ',', '.', '/',
 "capslock"}
 
+-- creates an empty button list
 function emptyButtonList()
   list = {}
   for i, key in ipairs(keys) do
@@ -15,38 +26,67 @@ function emptyButtonList()
   return list
 end
 
-function createButtonList()
-  keysPressed[1] = emptyButtonList()
-  keysPressed[2] = emptyButtonList()
-  keysPressed[3] = emptyButtonList()
+-- creates an empty button list that can also keep track of time pressed
+function emptyPressedButtonList ()
+  list = {}
+  for i, key in ipairs(keys) do
+    list[key] = {}
+    list[key].pressed = false
+    list[key].pressTime = 0
+  end
+  return list
 end
 
+
+-- creates the buttonList variable that keeps track of any keyPress
+function createButtonList(keyList)
+  keyList = keyList or keys
+  keysPressed = {}
+  keysPressed.keyPressStarted = emptyButtonList()
+  keysPressed.KPS = emptyButtonList()
+  keysPressed.keyPressContinued = emptyPressedButtonList()
+  keysPressed.KPC = emptyPressedButtonList()
+  keysPressed.keyPressReleased = emptyButtonList()
+  keysPressed.KPR = emptyButtonList()
+  return keysPressed
+end
+
+-- used so that all keyPresses get moved from started to continued
 function moveList(list)
-  listToEmpty = list[1]
-  list[1] = emptyButtonList()
-  list[3] = emptyButtonList()
+  listToEmpty = list.keyPressStarted
+  list.keyPressStarted = emptyButtonList()
+  list.keyPressReleased = emptyButtonList()
   for i, key in ipairs(keys) do
     if listToEmpty[key] then
-      list[2][key] = true
+      list.keyPressContinued[key].pressed = true
     end
   end
   listToEmpty = nil
   return list
 end
 
-function getInput()
+-- the actual method for updating the buttonList variable
+function getInput(keysPressed)
+  keysPressed = keysPressed or createButtonList()
   keysPressed = moveList(keysPressed)
   for i, key in ipairs(keys) do
     if love.keyboard.isScancodeDown(key) then
-      if not keysPressed[2][key] then
-        keysPressed[1][key] = true
+      if keysPressed.keyPressContinued[key].pressed then
+        keysPressed.keyPressContinued[key].pressTime =
+         keysPressed.keyPressContinued[key].pressTime + 1
+      else
+        keysPressed.keyPressStarted[key] = true
       end
     else
-      if keysPressed[2][key] then
-        keysPressed[3][key] = true
-        keysPressed[2][key] = false
+      if keysPressed.keyPressContinued[key] then
+        keysPressed.keyPressReleased[key] = true
+        keysPressed.keyPressContinued[key].pressed = false
+        keysPressed.keyPressContinued[key].pressTime = 0
       end
     end
   end
+  keysPressed.KPS = keysPressed.keyPressStarted
+  keysPressed.KPC = keysPressed.keyPressContinued
+  keysPressed.KPR = keysPressed.keyPressReleased
   return keysPressed
 end
--- gameFiles/PoolBuilderSim/main.lua
+++ gameFiles/PoolBuilderSim/main.lua
@@ -1,13 +1,29 @@
 require("inputHelper")
 
 function love.load()
-  createButtonList()
-end
-
-function love.draw()
+  buttonList = createButtonList()
   print("WOW")
 end
 
+function love.draw()
+  buttonList = getInput(buttonList)
+  printline = ""
+  if buttonList.KPC.w.pressed then
+    printline = printline .. "moving forward "
+  end
+  if buttonList.KPC.s.pressed then
+    printline = printline .. "moving backward "
+  end
+  if buttonList.KPC.a.pressed then
+    printline = printline .. "moving left "
+  end
+  if buttonList.KPC.d.pressed then
+    printline = printline .. "moving right"
+  end
+  love.graphics.print(printline, 50, 50)
+  love.graphics.print(buttonList.KPC.a.pressTime, 50, 100)
+end
+
 function love.update()
-  
+
 end
