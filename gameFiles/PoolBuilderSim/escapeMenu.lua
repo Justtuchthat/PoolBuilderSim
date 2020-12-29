fbd4aee4e4cdac511ae15248494313a65c8666a4
Justtuchthat <rutgerwuijster@hotmail.com>
2020-12-29 21:09:04 +0100

    Added return to main menu option

--- gameFiles/PoolBuilderSim/escapeMenu.lua
+++ gameFiles/PoolBuilderSim/escapeMenu.lua
@@ -1,7 +1,7 @@
 -- escapeMenu.lua
 -- created by Justtuchthat
 -- first created on 19-11-2020
--- last edited on 28-12-2020
+-- last edited on 29-12-2020
 -- used for creating the escape menu
 
 saveButtonMode = {}
@@ -12,12 +12,17 @@ local function drawEscapeOverlay()
   love.graphics.rectangle("fill", 0, 0, width, height)
 end
 
-local function saveButton()
+local function saveButtonFunction()
   changeMode('saveMenu')
 end
 
+local function exitButtonFunction()
+  changeMode('mainMenu')
+end
+
 local function resizeButtonReplacement(newWidth, newHeight)
-  saveButtonMode.loc = newLocationObject(newWidth/2, 100)
+  saveButton.loc.x = newWidth/2
+  exitButton.loc.x = newWidth/2
 end
 
 function setupEscapeMenu()
@@ -25,10 +30,11 @@ function setupEscapeMenu()
   modes.escapeMenu.draw:addFunction(renderGame)
   modes.escapeMenu.draw:addFunction(drawEscapeOverlay)
   width, height, _ = love.window.getMode()
-  saveButtonMode = newButton('text', newLocationObject(width/2, 100), 'Save game', {1, 1, 1}, {0.2, 0.8, 0.2})
-  saveButtonMode.pressAction:addFunction(saveButton)
-  modes.escapeMenu.menuItems:addMenuItem(saveButtonMode)
-
-
-
+  newResizeFunction(resizeButtonReplacement)
+  saveButton = newButton('text', newLocationObject(width/2, 100), 'Save game', {1, 1, 1}, {0.2, 0.8, 0.2})
+  exitButton = newButton('text', newLocationObject(width/2, 120), 'Exit to main menu', {1,1,1}, {0.2, 0.8, 0.2})
+  saveButton.pressAction:addFunction(saveButtonFunction)
+  exitButton.pressAction:addFunction(exitButtonFunction)
+  modes.escapeMenu.menuItems:addMenuItem(saveButton)
+  modes.escapeMenu.menuItems:addMenuItem(exitButton)
 end
