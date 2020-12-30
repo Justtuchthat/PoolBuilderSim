58cb646687368f6e6538a3f8499436fd5c885055
Justtuchthat <rutgerwuijster@hotmail.com>
2020-12-30 18:52:52 +0100

    fixed typo in require.lua

--- gameFiles/PoolBuilderSim/require.lua
+++ gameFiles/PoolBuilderSim/require.lua
@@ -1,7 +1,7 @@
 -- require.lua
 -- created by Justtuchthat
 -- first created on 05-10-2020
--- last edited on 29-12-2020
+-- last edited on 30-12-2020
 -- this is used to import all files in one require statement
 -- this is done to clean up the main file
 
@@ -9,7 +9,7 @@ require("inputHelper")
 require("textInputItem")
 require("gameRenderer")
 require("gameInit")
-require("poolEdgechecker")
+require("poolEdgeChecker")
 require("controlSetup")
 require("settingsSetup")
 require("buttonClass")
